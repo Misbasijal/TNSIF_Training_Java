0973c29691537b4f7dded0e524972a4079c301a7
misba <misba@Pikachu>
2026-07-23 14:22:02 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
@@ -0,0 +1,37 @@
+package org.tnsif.acc.c2tc.oops;
+
+class Human{
+//	variables
+		private String name;
+		private int age;
+	
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getName() {
+			return name;
+		}
+	
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public int getAge() {
+			return age;
+		}
+	}
+public class EncapsulationDemo {
+	
+	
+	public static void main (String[] args) {
+		Human obj=new Human();
+		obj.setName("Misba");
+		obj.setAge(21);
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+		 
+		obj.setName("Hema");
+		obj.setAge(34);
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+	}
+}
