16eb5c8e32cf1991e5a8968008b59b12d8c14661
misba <misba@Pikachu>
2026-07-23 15:06:42 +0530

    first commit

--- TNSIF_Training_Java/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
@@ -2,25 +2,24 @@ package org.tnsif.acc.c2tc.oops;
 
 class Human{
 //	variables
-		private String name;
-		private int age;
+	private String name;
+	private int age;
 	
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getName() {
-			return name;
-		}
-	
-		public void setAge(int age) {
-			this.age = age;
-		}
-		public int getAge() {
-			return age;
-		}
+	public void setName(String name) {
+		this.name = name;
 	}
-public class EncapsulationDemo {
+	public String getName() {
+		return name;
+	}
 	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+}
+public class EncapsulationDemo {
 	
 	public static void main (String[] args) {
 		Human obj=new Human();
