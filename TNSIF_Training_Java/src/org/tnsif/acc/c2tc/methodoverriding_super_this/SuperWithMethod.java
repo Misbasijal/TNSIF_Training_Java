36dd7f9648a3b66630a2f6cef5844356a3d739be
misba <misba@Pikachu>
2026-07-29 13:00:02 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/methodoverriding_super_this/SuperWithMethod.java
@@ -0,0 +1,29 @@
+package org.tnsif.acc.c2tc.methodoverriding_super_this;
+
+class Payment1{
+	void process() {
+		System.out.println("Processing payment using standard gateway");
+	}
+}
+
+class GPay extends Payment1{
+	
+	void process() {
+		System.out.println("Processing payment via GPay");
+	}
+	
+	void completePayment() {
+		super.process();
+		process();
+	}
+}
+
+public class SuperWithMethod {
+
+	public static void main(String[] args) {
+		GPay obj = new GPay();
+		obj.completePayment();
+
+	}
+
+}
