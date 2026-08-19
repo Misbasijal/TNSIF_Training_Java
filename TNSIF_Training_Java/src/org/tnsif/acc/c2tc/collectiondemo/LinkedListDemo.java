2255ba3b5dde01d7fe0305d6c4229e5ee3df9a5e
misba <misba@Pikachu>
2026-08-19 12:21:54 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/collectiondemo/LinkedListDemo.java
@@ -0,0 +1,39 @@
+package org.tnsif.acc.c2tc.collectiondemo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		List<String> fruits = new LinkedList<>();//if we use arraylist instead of list it will fetch all the methods from list as well as arraylist
+		fruits.add("Apple");
+		fruits.add("Mango");
+		fruits.add("Dragon Fruit");
+		
+		System.out.println(fruits);
+		System.out.println("Second Fruit : " +fruits.get(1));
+		fruits.set(1,"Cherry");
+		System.out.println(fruits);
+		fruits.remove(2);
+		System.out.println("After removing : "+fruits);
+		
+		if(fruits.contains("Mango"))
+		{
+			System.out.println("Requested fruit is present");
+		}else {
+			System.out.println("Requested fruit is not present");
+		}
+		
+		//Using Iterable
+		Iterable<String> iterableFruits = fruits;
+		System.out.println("Traversing using Iterable:" +iterableFruits);
+		
+		//using for each loop also you can travese
+		for(String fruit:fruits) {
+			System.out.println(fruit);
+		}
+
+	}
+
+}
